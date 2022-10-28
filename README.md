8b10628e49c821c31fec3ce732798893340f6fe0
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-10-28 09:03:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a react starter with tailwind support
+
+For first time usage, use 'npm install' to install the dependencies
+Then 'npm start' to run
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold capitalize">
+        This is react starter with tailwind
+      </p>
+    </div>
+  );
+};
+
+export default App;
